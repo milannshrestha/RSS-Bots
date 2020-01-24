5a3df4ff91b229d0cd5d0b33b677f2f004ff4b30
Milann SHRESTHA <31887847+milannshrestha@users.noreply.github.com>
2020-01-24 09:13:43 +0545

    Update rss-twitter-usr.py
    To RocketChat via Webhook

--- rss-twitter-usr.py
+++ rss-twitter-usr.py
@@ -6,6 +6,14 @@ from time import sleep
 
 hook = ("Webhook URL") 
 
+'''
+If you created a feed for a user, it will look like this: https://twitrss.me/twitter_user_to_rss/?user=[USERNAME]
+
+And if the feed is for a keyword / phrase or hash, it will look like this: http://twitrss.me/twitter_search_to_rss/?term=[SEARCH TERM]
+http://twitrss.me/twitter_search_to_rss/?term=[SEARCH TERM]
+'''
+
+
 parse = feedparser.parse('https://twitrss.me/twitter_user_to_rss/?user=<username>') #for Twitter RSS feed of particular user
 for i in parse.entries:
         l = (i.link)
